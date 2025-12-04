fc68869f8dae666ef770c37432658cdabd735e57
thk7964 <yjs7894@gmail.com>
2025-12-04 14:16:15 +0900

    lv 3: N+1 문제 명세를 잘못봐서 수정

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -6,17 +6,20 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    @EntityGraph(attributePaths = {"user"})
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
 
-    @EntityGraph(attributePaths = {"user"})
-    Optional<Todo> findTodoById(Long todoId);
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user " +
+            "WHERE t.id = :todoId")
+    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
 }
